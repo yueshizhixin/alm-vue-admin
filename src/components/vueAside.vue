e0ee9cb0f5ae067b9f7006099751c3e9923d16ae
yueshizhixin <yueshizhixin@163.com>
2019-07-31 17:52:39 +0800

    v0.0.3 html

--- src/components/vueAside.vue
+++ src/components/vueAside.vue
@@ -1,14 +1,37 @@
 <template>
   <div style="width: calc(100%);">
-    <div v-for="(item,index) of menu" :key="index" :class="{'active':item.chosen===1}"
-         class="divbg0 menu1 hvr-sweep-to-right">
-      <i :class="item.icon">&nbsp;</i>&nbsp;
+    <div style="width: calc(100%);">
+      <div v-for="(item,index) of menu" :key="index" :class="{'active':item.chosen===1}"
+           class="divbg0 menu1 hvr-sweep-to-right">
+        <i :class="item.icon">&nbsp;</i>&nbsp;
         {{item.name}}
-        <!--<div v-for="c of item.children">-->
-          <!--{{c.name}}-->
-        <!--</div>-->
-
+      </div>
     </div>
+
+    <div style="width: calc(100%);">
+      <ul class="divbg0 " style=" width: calc(100% - 60px);font-size: 18px;
+        line-height: 20px;
+        margin-bottom: 0px;
+        margin-top: 6px;
+        padding: 14px 0px;
+        margin-left: 3px;
+        font-weight: 100;list-style-type:none;
+        box-shadow: 0 0px 3px rgba(26, 26, 26, .1);">
+        <li v-for="(item,index) of menu" :key="index" :class="{'active':item.chosen===1}"
+            class=" ">
+          <div class="hvr-sweep-to-right"><i :class="item.icon">&nbsp;</i>&nbsp;
+            {{item.name}}
+          </div>
+          <ul v-for="c of item.children" class="hvr-sweep-to-right" style="list-style-type:none;
+              margin-top: 14px;">
+            <li>
+              {{c.name}}
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+
   </div>
 
 </template>
@@ -16,10 +39,9 @@
 <script>
   export default {
     name: "vueAside",
-    props:['menu'],
+    props: ['menu'],
     data() {
-      return {
-      }
+      return {}
     },
     mounted() {
       // this.aniShow = true
@@ -33,7 +55,7 @@
     },
     methods: {
       //显示3级菜单
-      showDepth3(){
+      showDepth3() {
 
       }
     }
@@ -52,13 +74,12 @@
     padding: 14px 27px;
     margin-left: 3px;
     font-weight: 100;
-    /*box-shadow:-0px 0px 10px red;*/
     box-shadow: 0 0px 3px rgba(26, 26, 26, .1);
   }
 
   .active {
-    background-color: #4a4266;
-    color: #fff;
+    /*background-color: #4a4266;*/
+    /*color: #fff;*/
   }
 
 </style>
