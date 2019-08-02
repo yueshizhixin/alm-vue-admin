27b91239e0cf26eec665ff707ae3b46f08b42de5
yueshizhixin <yueshizhixin@163.com>
2019-08-02 15:32:59 +0800

    v0.0.4 html

--- src/components/vueAside.vue
+++ src/components/vueAside.vue
@@ -8,24 +8,31 @@
       </div>
     </div>
 
-    <div style="width: calc(100%);">
-      <ul class="divbg0 " style=" width: calc(100% - 60px);font-size: 18px;
-        line-height: 20px;
-        margin-bottom: 0px;
-        margin-top: 6px;
-        padding: 14px 0px;
-        margin-left: 3px;
-        font-weight: 100;list-style-type:none;
-        box-shadow: 0 0px 3px rgba(26, 26, 26, .1);">
+    <div style="">
+      <ul class=""
+          style="list-style-type:none;padding-left: 0;margin-top: 6px;margin-left:  3px;width:calc(100% - 3px);">
         <li v-for="(item,index) of menu" :key="index" :class="{'active':item.chosen===1}"
             class=" ">
-          <div class="hvr-sweep-to-right"><i :class="item.icon">&nbsp;</i>&nbsp;
+          <div class="hvr-sweep-to-right divbg0"
+               style="width:calc(100% - 57px);
+               font-size: 18px;
+               font-weight: 100;
+               line-height: 20px;
+               padding: 14px 27px;margin-bottom: 6px;box-shadow: 0 0px 3px rgba(26, 26, 26, .1);"><i
+            :class="item.icon">&nbsp;</i>&nbsp;
             {{item.name}}
           </div>
-          <ul v-for="c of item.children" class="hvr-sweep-to-right" style="list-style-type:none;
-              margin-top: 14px;">
+          <ul v-for="c of item.children" class="hvr-sweep-to-right divbg0" style="list-style-type:none;
+              font-size: 16px;
+              width: calc(100% - 57px);
+              display: block;
+               font-weight: 100;
+               line-height: 20px;
+               padding: 10px 27px;
+               padding-left: 54px;
+               margin-bottom: 6px;box-shadow: 0 0px 3px rgba(26, 26, 26, .1);">
             <li>
-              {{c.name}}
+              &nbsp;{{c.name}}
             </li>
           </ul>
         </li>
